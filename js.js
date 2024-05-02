7c1a03f6549f31809bdd0792337685353ae54fdc
ashishmishra112 <151738834+ashishmishra112@users.noreply.github.com>
2024-05-02 08:37:07 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+function ashish()
+{
+    document.getElementById('ashish').src = 'images/aa.jpeg';
+}
+
+
+function chitra() 
+{
+    document.getElementById('ashish').src = 'images/bb.png';
+}
+
+
+function arpita() 
+{
+    document.getElementById('ashish').src = 'images/cc.png';
+}
+
+   
+function samridhi() 
+{
+    document.getElementById('ashish').src = 'images/dd.webp';
+}
+
+
+function pratham() 
+{
+    document.getElementById('ashish').src = 'images/ee.jpg';
+}
